5dfb6ffdca33ce79594505cdc69bd39845920d2d
CMcCown <connermccown94@gmail.com>
2022-06-28 20:40:23 -0600

    updated README

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ A scheduling app that allows you to track your to do's with color changing rows 
 ### Screenshot
 ![_C__Users_conne_Desktop_projects_workday-scheduler_index html](https://user-images.githubusercontent.com/6404921/154854850-75ba29f2-44fa-4e90-896c-d1ad4c796fa1.png)
 
+### Github
+https://github.com/mccrown/workday-scheduler
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-// date at top of page
+
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
-// tasks objects for localStorage
+
 var tasks = {
     "9": [],
     "10": [],
@@ -15,30 +15,30 @@ var tasks = {
     "17": []
 };
 
-// save tasks to storage
+
  var saveTasks = function() {
      localStorage.setItem("tasks", JSON.stringify(tasks));
  }
 
- // get tasks from storage
+
  var getTasks = function() {
      
     var loadedTasks = JSON.parse(localStorage.getItem("tasks"));
      if(loadedTasks) {
          tasks = loadedTasks
 
-         // for each key/value pair in tasks create a task
+         
          $.each(tasks, function(hour,task){
              var hourDiv = $("#" + hour);
              createTask(task, hourDiv);
          })
      }
 
-     // check if past/current/future
+     
      auditTasks()
  }
 
- // create task in corresponding hour row
+ 
  var createTask = function(taskText, hourDiv) {
 
     var taskDiv = hourDiv.find(".task");
@@ -48,14 +48,14 @@ var tasks = {
         taskDiv.html(taskP);
  }
 
- // update background based on time of day
+
  var auditTasks = function() {
 
     var currentHour = moment().hour();
     $(".task-info").each(function(){
         var elementHour = parseInt($(this).attr("id"));
 
-        // past, present and future check
+        
         if (elementHour < currentHour) {
             $(this).removeClass(["present", "future"]).addClass("past");
         }
@@ -69,59 +69,59 @@ var tasks = {
 
  };
 
- // replace textarea with a p element and persists in localStorage
+
  var replaceTextarea = function(textareaElement) {
     var taskInfo = textareaElement.closest(".task-info");
     var textArea = taskInfo.find("textarea");
 
-    // get time and task
+    
     var time = taskInfo.attr("id");
     var text = textArea.val().trim();
 
-   // persist the data
+   
    tasks[time] = [text];
    saveTasks();
 
-   //replace textArea element with a p element
+   
    createTask(text, taskInfo);
 }
 
-// click handlers
 
-// tasks
+
+
 $(".task").click(function() {
 
-   // save the other tasks if they've already been clicked
+   
    $("textarea").each(function() {
        replaceTextarea($(this));
    })
 
-   //convert to a textarea element if the time hasn't passed
+  
    var time = $(this).closest(".task-info").attr("id");
    if (parseInt(time) >= moment().hour()) {
 
-       //create a textInput element that includes the current task
+       
        var text = $(this).text();
        var textInput = $("<textarea>")
        .addClass("form-control")
        .val(text);
 
-       // add the textInput element to the parent div
+       
        $(this).html(textInput);
        textInput.trigger("focus");
    }
 })
 
-// save button click handler
+
 $(".saveBtn").click(function() {
     replaceTextarea($(this));
 })
 
-// update task backgrounds on the hour
-timeToHour = 3600000 - today.milliseconds();  // check how much time is left until the next hour
+
+timeToHour = 3600000 - today.milliseconds();  
 setTimeout(function () {
     setInterval(auditTasks, 3600000)
 }, timeToHour);
 
-// get the tasks from localStorage on load
+
 getTasks();
